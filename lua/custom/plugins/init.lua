22ef093cc39df377c9cc99aa8d7747fc4f98be70
Rui Xuan <ruixuantan@yahoo.com.sg>
2025-02-02 19:15:40 +0800

    Add csharpier

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -216,8 +216,8 @@ return {
         clangd = {},
         -- gopls = {},
         ruff = {},
-        rust_analyzer = {},
         pyright = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -279,7 +279,8 @@ return {
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
+        'stylua',
+        'csharpier',
         'fixjson',
         'yamlfix',
       })
